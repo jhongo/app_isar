ada375b534c9bd6fd13097141cfa4981b62aab39
jhongo <jhon_gom22@outlook.com>
2024-01-19 22:37:24 -0500

    Desing view for create routine

--- lib/presentation/routine/routine_screen.dart
+++ lib/presentation/routine/routine_screen.dart
@@ -9,20 +9,22 @@ class CreateRoutine extends StatefulWidget {
 
 class _CreateRoutineState extends State<CreateRoutine> {
 
-  TextEditingController _TextEditngController = TextEditingController();
+  final TextEditingController _titleEditngController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
+  final TextEditingController _categoryController = TextEditingController();
+
 
   List<String> categories = ['work','school', 'home'];
+  List<String> days = ['monday','tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+  String? dayValue = 'monday';
   String? categoryValue = 'work';
+
+  TimeOfDay selectedTime = TimeOfDay.now();
 
   @override
   void initState() {
     super.initState();
-
-    final text = _TextEditngController.text;
-    print(text);
-    setState(() {
-      
-    });
   }
 
 
@@ -62,19 +64,85 @@ class _CreateRoutineState extends State<CreateRoutine> {
                       }),
                   ),
                     IconButton(
-                      onPressed:(){}, 
+                      onPressed:(){
+                        showDialog(
+                          context: context, 
+                          builder: (context) {
+                            return AlertDialog(
+                              title: Text('Add Category'),
+                              content: TextField(
+                                controller: _categoryController,
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  }, 
+                                  child: Text('Cancel')),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  }, 
+                                  child: Text('Save'))
+                              ],
+                            );
+                          });
+                      }, 
                       icon: const Icon(Icons.add))
                 ],
               ),
 
               Text('Title'),
               TextFormField(
-                controller: _TextEditngController,
+                controller: _titleEditngController,
                 decoration: const InputDecoration(
                   hintText: 'Title',
                   border: OutlineInputBorder()
                 ),
               ),
+              SizedBox(height: 20,),
+              Text('Start Time'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.8,
+                    child: TextFormField(
+                      controller: _timeController,
+                      enabled: false,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: (){
+                      _selectedTime(context);
+                    }, 
+                    icon: const Icon(Icons.timer))
+                ],
+              ),
+
+              SizedBox(height: 20,),
+              Text('Day'),
+              DropdownButton(
+                isExpanded: true,
+                value: dayValue,
+                items: days.map((e) => DropdownMenuItem(
+                  value: e,
+                  child: Text(e),
+                )).toList() , 
+                onChanged:(value) {
+                  setState(() {
+                  dayValue = value;
+                  });
+                },
+                ),
+
+            Align(
+              alignment: Alignment.center,
+                child: FilledButton(
+                  onPressed: (){}, 
+                  child: Text('SAVE')
+                  ),
+              )  
         
             ],
           ),
@@ -82,4 +150,22 @@ class _CreateRoutineState extends State<CreateRoutine> {
       ),
     );
   }
+
+  _selectedTime(BuildContext context) async {
+  final TimeOfDay? timeofDay = await showTimePicker(
+    context: context, 
+    initialTime: selectedTime,
+    initialEntryMode: TimePickerEntryMode.dial
+    );
+
+  if(timeofDay != null && timeofDay != selectedTime) {
+    selectedTime = timeofDay;
+    setState(() {
+      _timeController.text = "${selectedTime.hour}:${selectedTime.minute} ${selectedTime.period.name}";
+    });
+  }
 }
+}
+
+
+
